4cd93784fb321e48f15bf276886044b8082b6735
Philippe <66671299+Philippe-dss@users.noreply.github.com>
2025-03-26 10:19:55 +0000

    25.03.26 - Changes made ot resemble paper

--- code/WhiskerPlot copy.py
+++ code/WhiskerPlot copy.py
@@ -4,45 +4,16 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-import scienceplots
 black = [0,0,0]
 red   = [1,0,0]
 blue  = [0,0,1]
-colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', 
-          '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-
-
-unique_labels = [
-    '\\textbf{CMB with Planck}',
-    '\\textbf{CMB without Planck}',
-    '\\textbf{Cepheids-SNIa}',
-    '\\textbf{GW related}',
-    '\\textbf{HII galaxies}',
-    '\\textbf{Lensing related; mass model-dependent}',
-    '\\textbf{Masers}',
-    '\\textbf{Miras-SNIa}',
-    '\\textbf{No CMB; with BBN}',
-    '\\textbf{Optimistic average}',
-    '\\textbf{Pl(k) + CMB lensing}',
-    '\\textbf{SNII}',
-    '\\textbf{Surface Brightness Fluctuations}',
-    '\\textbf{TRGB-SNIa}',
-    '\\textbf{Tully-Fisher Relation}',
-    '\\textbf{Ultra Conservative - no cepheids no lensing}',
-    ]
 
 #+++++++++++++++++++++++++++++
 from matplotlib import rcParams
 config = {
-	"mathtext.fontset":'stix',
-    'font.family':'Times New Roman',
-    'text.usetex': True,
-    #'text.latex.preamble': r'\usepackage{amsmath}\boldmath',
-
+	"mathtext.fontset":'dejavusans'
 	}
-#plt.rcParams['text.latex.preamble'] = r'\usepackage{mathastext}'
 rcParams.update(config)
-plt.style.use('science')
 #+++++++++++++++++++++++++++++
 
 class ErrorLinePloter:
@@ -183,9 +154,9 @@ for i in range(nr):
 paras=[]
 for i in range(nr):
     if Hl[i]==Hp[i]:
-        paras.append(aut[i]+' '+str(H0[i])+'${\pm}$'+str(Hp[i]))
+        paras.append(aut[i]+str(H0[i])+'${\pm}$'+str(Hp[i]))
     else:
-        paras.append(aut[i]+' '+str(H0[i])+'$^{+'+str(Hp[i])+'}_{-'+str(Hl[i])+'}$')
+        paras.append(aut[i]+str(H0[i])+'$^{+'+str(Hp[i])+'}_{-'+str(Hl[i])+'}$')
 
 #---------------------------data-------------------------------------
 all_data = []
@@ -193,9 +164,7 @@ for i in range(nr):
     all_data.append({'ml':H0[i],'e1_sig':[Hp[i],-Hl[i]]})
 
 #---------------------------style-------------------------------------
-unique_lbl = np.unique(lbl)
-n_lbl = len(unique_lbl)
-pos_num = nr + 2 * n_lbl
+pos_num = nr
 
 positions=[]
 labels=[]
@@ -203,66 +172,40 @@ for i in range(pos_num+3):
     positions.append(i)
     labels.append('')
 
-#unique_labels=[]
-#unique_pos=[]
+unique_labels=[]
+unique_pos=[]
 
 #---------------------------plot-------------------------------------#
 pdf = PdfPages('H0whisker.pdf')
 
 plt.rcParams['figure.figsize'] = (5,6.8)
 
+plt.text(77.5,nr-nind+1.4,"Indirect",size=9)
+plt.text(77.5,nr-nind-0.1,"Direct",size=9)
+plt.text(74.7,nr,"${H_0}\,$[km$\,$s$^{-1}\,$Mpc$^{-1}$]",size=9)
+
+### Plot the vertical bars for reference: R20 vs CMB
+plt.bar(73.2, 100, width=2.6, facecolor = 'cyan', alpha = 0.15)
+plt.bar(67.27, 100, width=1.2, facecolor = 'pink', alpha = 0.25)
+
 ### Plot each data point with attached label
 j=0
 k=nind
-current_label = None
-label_pos = 0
-color_index = -1  # Add this to track which color to use
-
-# Container for the special lbl labels:
-lbl_positions = []
-lbl_labels = []
-direct_position = 0
-direct_check = True
-
+n_lbl = len(np.unique(lbl))
 for i in range(len(paras)):
-    # Check that the label is the correct one.
-    # If the lbl value is different, the method of measurement changes,
-    # so we need to change the label value.
-    if lbl[i] != current_label:
-        label_pos += 1
-        current_label = lbl[i]
-        if method[i]=='Indirect':
-            lbl_positions.append(pos_num-j+1-label_pos)
-            #lbl_labels.append(lbl[i])
-        else:
-            lbl_positions.append(pos_num-k-label_pos)
-            #lbl_labels.append('!!!TO BE REPLACED LATER ON IN CODE!!!')
-            #lbl_labels.append(f'$\textbf{lbl[i]}$')
-        label_pos += 1
-        color_index = (color_index + 1) % len(colors)# To cycle through the colors, if necessary.
-        
-    current_color = colors[color_index]
-    # Plot the data point:
     if method[i]=='Indirect':
-        elp = ErrorLinePloter(all_data[i],position=pos_num-j+1-label_pos)
-        j += 1
+      elp = ErrorLinePloter(all_data[i],position=nr-j+1)
+      j += 1
     elif method[i]=='Direct':
-        if direct_check:
-            direct_position = pos_num-k-label_pos+2
-            direct_check = False
-        elp = ErrorLinePloter(all_data[i],position=pos_num-k-label_pos)
-        k += 1
+      elp = ErrorLinePloter(all_data[i],position=nr-k)
+      k += 1
     labels[elp.position]=paras[i]
-    elp.set_props(
-          0.8,'-',current_color,
-          0.7,current_color,0.8,
-          'marker',
-          mpsize=2.0,
-          mpcolor=current_color,
-          mshape='o')
+    elp.set_props(0.8,'-',black,
+          0.7,black,0.8,
+          'marker',mpsize=2.0,mpcolor=black,mshape='o')
     elp.plot()
 
-elp = ErrorLinePloter({'ml':0.0,'e1_sig':[100.,-100.]},position=direct_position)
+elp = ErrorLinePloter({'ml':0.0,'e1_sig':[100.,-100.]},position=nr-nind+1)
 labels[elp.position]=''
 elp.set_props(0.8,'-.',black,
        0.7,black,0.8,
@@ -270,46 +213,15 @@ elp.set_props(0.8,'-.',black,
 elp.plot()
 
 plt.tick_params(axis='x',labelsize=8)
-plt.tick_params(axis='y',labelsize=0)
+plt.tick_params(axis='y',labelsize=4.4)
 plt.xticks([i for i in range(60,100,5)])#,fontweight='semibold')
+
 plt.xlim(61,82.5)
 
 plt.ylim(positions[0],positions[-1])
 
-tick_position = np.concatenate((positions,lbl_positions))
-tick_label = np.concatenate((labels,unique_labels))
+plt.yticks(positions,labels)#,fontweight='semibold')
 
-#print(len(tick_position), len(tick_label), len(positions), len(labels), len(unique_labels), len(lbl_positions))
-
-
-plt.yticks(tick_position, tick_label,)#fontweight='semibold')
-
-
-# Apply formatting: boldface and size for some, normal and size for others
-yticklabels = plt.gca().get_yticklabels()
-for tick, label in zip(yticklabels, tick_label):
-    #tick.set_text(label)
-    if '20' not in label:
-        # change the colour to white:
-        tick.set_color('k')
-        tick.set_fontsize(5)
-    
-    #if 'textbf' in label:
-    
-        #tick.set_fontsize(0.0)
-    else:
-        tick.set_fontsize(3)
-plt.gca().set_yticklabels(yticklabels)
-
-plt.text(77.5,direct_position+1.,"Indirect",size=9)
-plt.text(77.5,direct_position-1.8,"Direct",size=9)
-plt.text(74.7,pos_num-3,"${H_0}\,$\n[km$\,$s$^{-1}\,$Mpc$^{-1}$]",size=10)
-
-### Plot the vertical bars for reference: R20 vs CMB
-plt.bar(73.2, 100, width=2.6, facecolor = 'cyan', alpha = 0.15)
-plt.bar(67.27, 100, width=1.2, facecolor = 'pink', alpha = 0.25)
-
-plt.minorticks_off()
 plt.tight_layout()
 pdf.savefig()
 pdf.close()
